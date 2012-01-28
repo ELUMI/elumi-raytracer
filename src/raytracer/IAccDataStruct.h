62e99b78c5dd496f2bfea34e08f420fe49bbf45a
Julian Lindblad <JulianLindblad@gmail.com>
2012-01-28 17:26:16 +0100

    Completed implementation on ArrayDataStruct

--- src/raytracer/IAccDataStruct.h
+++ src/raytracer/IAccDataStruct.h
@@ -8,6 +8,8 @@
 #ifndef IACCDATASTRUCT_H_
 #define IACCDATASTRUCT_H_
 
+#include <stdexcept>
+
 #include "utilities/Triangle.h"
 #include "utilities/Ray.h"
 
@@ -16,9 +18,20 @@ namespace raytracer {
 class IAccDataStruct {
 public:
   struct IntersectionData {
+    IntersectionData(Triangle* triangle, vec3 interPoint, vec3 normal) {
+      IntersectionData::triangle = triangle;
+      IntersectionData::interPoint = interPoint;
+      IntersectionData::normal = normal;
+    };
+
     Triangle* triangle;
     vec3 interPoint;
     vec3 normal;
+  };
+
+  class NoIntersectionException : public std::runtime_error {
+  public:
+    NoIntersectionException() : std::runtime_error("No intersection!") {};
   };
 
   IAccDataStruct();
