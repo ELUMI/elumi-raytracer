e345805a629ddc0d3a4f4da6a0750aee8e3d1af6
Julian Lindblad <JulianLindblad@gmail.com>
2012-03-04 19:10:04 +0100

    Local tonemaper is finally generating correct image.

--- src/raytracer/PostEffectImpl/ReinhardOperator.cpp
+++ src/raytracer/PostEffectImpl/ReinhardOperator.cpp
@@ -35,45 +35,42 @@ float* ReinhardOperator::run(float* color_buffer, int length) {
 
   RGB2XYZ = transpose(RGB2XYZ);
 
-//  vec3 one(1.0f, 1.0f, 1.0f);
-//
-//  vec3 res = RGB2XYZ * one;
-//  cout << "res: " << res.x << ", " << res.y << ", " << res.z << endl;
-//
-//  mat3 XYZ2RGB = mat3(2.5651,   -1.1665,   -0.3986,
-//                     -1.0217,    1.9777,    0.0439,
-//                      0.0753,   -0.2543,    1.1892);
-//
-//  XYZ2RGB = transpose(XYZ2RGB);
-//
-//  vec3 res2 = XYZ2RGB * res;
-//  cout << "res2: " << res2.x << ", " << res2.y << ", " << res2.z << endl;
-//
-//
-//  assert(false);
-
   int pixels = length / 4;
 
-  // Convert to XYZ OBS! There is a difference between capital and no capital letters in this color space.
   vec3 buffer[pixels];
   cout << "ReinhardOperator: Assuming buffer format RGBA." << endl;
   for( int i = 0; i < length; i += 4) {
     vec3 v = vec3(color_buffer[i],
                   color_buffer[i+1],
                   color_buffer[i+2]);
-    v = RGB2XYZ * v;    // TODO konvertera till Yxy (jmf. kŠllkod)
+
+    // Convert from RGB to XYZ
+    v = RGB2XYZ * v;
+
+    // Convert from XYZ to Yxy
+    float nom = v.x + v.y + v.z;
+    if(nom > 0.f) {
+      float Y = v.y;        // Y
+      float x = v.x / nom;  // x
+      float y = v.y / nom;  // y
+      v.x = Y;
+      v.y = x;
+      v.z = y;
+    } else {
+      v.x = 0;
+      v.y = 0;
+      v.z = 0;
+    }
+
     buffer[i/4] = v;
   }
 
   float log_avg = logAverage(buffer, pixels);
   float scale_factor = key / log_avg;
-  cout << "ReinhardOperator: log_avg: " << log_avg << endl;
-  cout << "ReinhardOperator: scale_factor: " << scale_factor << endl;
+//  cout << "ReinhardOperator: log_avg: " << log_avg << endl;
+//  cout << "ReinhardOperator: scale_factor: " << scale_factor << endl;
   for(int i = 0; i < pixels; i++) {       // Eq. 2
-    buffer[i].y *= scale_factor;
-//    cout << "(" << buffer[i].x << ", " << buffer[i].y << ", " << buffer[i].z << "), ";
-//    if(i % 3 == 0)
-//      cout << endl;
+    buffer[i].x *= scale_factor;
   }
 
 
@@ -85,12 +82,12 @@ float* ReinhardOperator::run(float* color_buffer, int length) {
     lmax2 *= lmax2;
   }
 
-  cout << "ReinhardOperator: lmax2: " << lmax2 << endl;
+//  cout << "ReinhardOperator: lmax2: " << lmax2 << endl;
   for(int i = 0; i < pixels; i++) {         // Eq. 4 (if white is big enough, eq 3)
-    buffer[i].y = buffer[i].y / (1.0f + buffer[i].y);
+    //buffer[i].x = buffer[i].x / (1.0f + buffer[i].x);
 
-    //buffer[i].y = (buffer[i].y * ( 1.0f + buffer[i].y / lmax2) /
-    //              (1.0f + buffer[i].y));
+    buffer[i].x = (buffer[i].x * ( 1.0f + buffer[i].x / lmax2)) /
+                  (1.0f + buffer[i].x);
   }
 
   mat3 XYZ2RGB = mat3(2.5651,   -1.1665,   -0.3986,
@@ -100,13 +97,28 @@ float* ReinhardOperator::run(float* color_buffer, int length) {
   XYZ2RGB = transpose(XYZ2RGB);
 
   for(int i = 0; i < length; i += 4) {
-    vec3 v = XYZ2RGB * buffer[i/4];
+    vec3 v = buffer[i/4];
+
+    // Convert from Yxy to XYZ
+    if(v.x > 0.f && v.y > 0.f && v.z > 0.f) {
+      float Y = v.x, x = v.y, y = v.z;
+      float X = (x * Y) / y;
+      float Z = (X / x) - X - Y;
+      v.x = X;
+      v.y = Y;
+      v.z = Z;
+    } else {
+      v.x = 0.f;
+      v.y = 0.f;
+      v.z = 0.f;
+    }
+
+    // Convert from XYZ to RGB
+    v = XYZ2RGB * v;
+
     color_buffer[i]    = v.x;
     color_buffer[i+1]  = v.y;
     color_buffer[i+2]  = v.z;
-    cout << "(" << v.x << ", " << v.y << ", " << v.z << "), ";
-        if((i/4) % 3 == 0)
-          cout << endl;
   }
 
   return color_buffer;
@@ -115,8 +127,8 @@ float* ReinhardOperator::run(float* color_buffer, int length) {
 float ReinhardOperator::getMaxValue(vec3* buf, int pixels) {
   float max = 0;
   for(int i = 0; i < pixels; i++) {
-    if(buf[i].y > max)
-      max = buf[i].y;
+    if(buf[i].x > max)
+      max = buf[i].x;
   }
   if(max == 0)    // Prevent division by zero.
     max = 1e20;
@@ -127,7 +139,7 @@ float ReinhardOperator::getMaxValue(vec3* buf, int pixels) {
 float ReinhardOperator::logAverage(vec3* buf, int pixels) { // Eq. 1
   double sum = 0;
   for(int i = 0; i < pixels; i++) {
-    sum += log(0.00001 + (double)buf[i].y);
+    sum += log(0.00001 + (double)buf[i].x);
   }
   double res = sum / (double)pixels;  // OBS! Wrong in paper! division by pixels
   return exp(res);                    // should be done before! exp-function
